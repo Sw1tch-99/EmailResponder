eb900ea3335142657e91bc0a6111b20acb4f179f
Tanishq Nigam <tnigam27@gmail.com>
2025-05-17 15:08:10 +0530

    Update StartApp.bat

--- StartApp.bat
+++ StartApp.bat
@@ -2,7 +2,7 @@ cd email-writer-sb
 cd src
 cd main
 cd java
-java EmailWriterSbApplication.java
+${env:GEMINI_URL}='https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent'; ${env:GEMINI_KEY}='AIzaSyA42vsYmeOBVZ9WbymjV0dmLpFabo4srqU'; & 'C:\Program Files\Java\jdk-21\bin\java.exe' '@C:\Users\KIIT\AppData\Local\Temp\cp_chatdazllzhw4a5x0fkr6v0i9.argfile' 'com.email.writer.EmailWriterSbApplication'
 cd..
 cd..
 cd..
